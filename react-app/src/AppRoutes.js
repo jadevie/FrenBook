047b83762f5e5dd58e200f58712375dd17fea55b
JadeVieTran <jadevtran@gmail.com>
2022-12-22 19:25:40 -0600

    Conditional rendering Splashpage vs Homepage

--- react-app/src/AppRoutes.js
+++ react-app/src/AppRoutes.js
@@ -1,13 +1,19 @@
+import { useSelector } from "react-redux";
 import { Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import LogoutButton from "./components/auth/LogoutButton";
+import HomePage from "./components/HomePage/HomePage";
 import SplashPage from "./components/SplashPage/SplashPage";
 
+
 export default function AppRoutes() {
+    const user = useSelector(state => state.session.user);
     return (
         <Switch>
             <Route path='/' exact={true} >
-                <SplashPage />
+                {!user ? <SplashPage /> : <HomePage />}
+            </Route>
+            <Route path='/logout' exact={true}>
                 <LogoutButton />
             </Route>
         </Switch>
---
+++ react-app/src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import styles from './HomePage.module.css';
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+            <div>HomePage</div>
+    
+        </>
+    );
+};
+
+export default HomePage;
