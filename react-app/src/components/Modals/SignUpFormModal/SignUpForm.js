65d7c97470b70949d7f0c2c70e3f415214895c41
JadeVieTran <jadevtran@gmail.com>
2022-12-29 15:59:43 -0600

    Change birthday input to MM/DD/YYYY

--- react-app/src/components/Modals/SignUpFormModal/SignUpForm.js
+++ react-app/src/components/Modals/SignUpFormModal/SignUpForm.js
@@ -12,13 +12,16 @@ const SignUpForm = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [gender, setGender] = useState('');
-    const [birthday, setBirthday] = useState('');
+    const [month, setMonth] = useState('01');
+    const [day, setDay] = useState('1');
+    const [year, setYear] = useState('2022');
     const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('');
     const user = useSelector(state => state.session.user);
     const dispatch = useDispatch();
 
     const onSignUp = async (e) => {
+        const birthday = `${year}-${month}-0${day}`;
         e.preventDefault();
         if (password === repeatPassword) {
             await dispatch(signUp(firstName, lastName, username, email, password, gender, birthday))
@@ -30,6 +33,13 @@ const SignUpForm = () => {
         }
         if (user) return <Redirect to='/' />;
     };
+
+    const months = [{ '01': 'Jan' }, { '02': 'Feb' }, { '03': 'Mar' }, { '04': 'Apr' }, { '05': 'May' }, { '06': 'Jun' }, { '07': 'Jul' }, { '08': 'Aug' }, { '09': 'Sep' }, { '10': 'Oct' }, { '11': 'Nov' }, { '12': 'Dec' }];
+
+    const days = [...Array(31).keys()].map(index => index + 1);
+
+    const years = [...Array(118).keys()].map(index => index + 1905).sort((a, b) => b - a);
+
 
 
     return (
@@ -78,12 +88,28 @@ const SignUpForm = () => {
                 </div>
                 <div>
                     <label>Birthday</label>
-                    <input
-                        type='text'
-                        name='birthday'
-                        onChange={e => setBirthday(e.target.value)}
-                        value={birthday}
-                    ></input>
+                    <select name='month' onChange={e => setMonth(e.target.value)} value={month}>
+                        {months.map((month, i) =>
+                            <option
+                                key={i}
+                                value={Object.keys(month)[0]}>{Object.values(month)[0]}
+                            </option>
+                        )}
+                    </select>
+                    <select name='day' onChange={e => setDay(e.target.value)} value={day}>
+                        {days.map((day, i) =>
+                            <option
+                                key={i}
+                                value={day}>{day}</option>
+                        )}
+                    </select>
+                    <select name='year' onChange={e => setYear(e.target.value)} value={year}>
+                        {years.map((year, i) =>
+                            <option
+                                key={i}
+                                value={year}>{year}</option>
+                        )}
+                    </select>
                 </div>
                 <div>
                     <label>Gender</label>
