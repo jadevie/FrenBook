9af64cc748992035077986f2687d16382ea81016
JadeVieTran <jadevtran@gmail.com>
2022-12-30 18:29:35 -0600

    Implement Like feature

--- react-app/src/components/HomePage/NewFeed/AllPosts/AllPosts.js
+++ react-app/src/components/HomePage/NewFeed/AllPosts/AllPosts.js
@@ -1,5 +1,5 @@
 import styles from './AllPosts.module.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPosts } from '../../../../store/posts';
 import CommentForm from './CommentForm/CommentForm';
@@ -9,16 +9,27 @@ import EditCommentForm from './EditCommentForm/EditCommentForm';
 
 
 const AllPosts = ({ user }) => {
+    const count = 0;
     const editComment = useSelector(state => state.commentDetails.comment);
     const posts = useSelector(state => state.posts);
     const allposts = posts.allPosts;
     const postArray = Object.values(allposts)
         .sort((a, b) => a.id < b.id ? 1 : -1);
     const dispatch = useDispatch();
+    const [likeCount, setLikeCount] = useState(count);
+    const [clicked, setClicked] = useState(false);
 
     useEffect(() => {
         dispatch(getPosts());
     }, [dispatch]);
+
+    const handleLike = e => {
+        e.preventDefault();
+        if (!clicked) setLikeCount(count + 1);
+        if (clicked) setLikeCount(likeCount - 1);
+        document.getElementById('like').className = styles.likeClicked;
+        setClicked(!clicked);
+    };
 
     return (
         <div>{postArray.map((post, i) =>
@@ -42,6 +53,34 @@ const AllPosts = ({ user }) => {
                     <div className={styles.postImageWrapper}>
                         {post.images.length ? post.images.map((image, i) => <img key={i} id='postImage' alt='' src={image.image_url} className={styles.image} onError={e => e.target.style.display = 'none'} />) : null}
                     </div>
+
+                    <div className={styles.postLikeCommentWrapper}>
+                        <div className={styles.postLikeInfo}>
+                            <div className={styles.like}><i className="fa-regular fa-thumbs-up"></i>
+                            </div>
+                            <div className={styles.count}>{likeCount}</div>
+                        </div>
+                        <div className={styles.postCommentInfo}>
+                            <div className={styles.count}>{`${post.comments.length} comments`}</div>
+                        </div>
+                    </div>
+
+                    <div className={styles.actionWrapper}>
+                        <button className={styles.actionBtnWrapper} onClick={handleLike}>
+                            <span className={!clicked ? styles.actionBtn : styles.likeClicked} id='like'>
+                                <div><i className="fa-regular fa-thumbs-up"></i></div>
+                                <div>Like</div>
+                            </span>
+                        </button>
+
+                        <button className={styles.actionBtnWrapper}>
+                            <span className={styles.actionBtn}>
+                                <div><i className="fa-regular fa-message"></i></div>
+                                <div>Comment</div>
+                            </span>
+                        </button>
+                    </div>
+
                     <div>
                         {post.comments.length ? post.comments.map((comment, i) =>
                             <div key={i} className={styles.comments}>
